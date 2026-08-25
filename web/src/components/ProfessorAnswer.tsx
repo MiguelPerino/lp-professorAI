0ec5d0a5c04b3b0f772b2871f844e6059870be45
Raphael Feijó Salles <feijoraphael1@gmail.com>
2026-08-25 19:02:11 -0300

    fix: polish Professor canary experience

---
+++ web/src/components/ProfessorAnswer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+export function ProfessorAnswer({ children }: { children: string }) {
+  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
+}
+
+export default ProfessorAnswer
