ed6200557a767bbc2624d90950cd679a6ca87542
Raphael Feijó Salles <199678983+RaphaelFeijoSalles@users.noreply.github.com>
2026-09-01 12:16:18 -0300

    Complete issue #9

--- web/src/components/ProfessorAnswer.tsx
+++ web/src/components/ProfessorAnswer.tsx
@@ -3,7 +3,9 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 export function ProfessorAnswer({ children }: { children: string }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{children}</ReactMarkdown>
+  const normalizedAnswer = children.replace(/\\n/g, '\n')
+
+  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{normalizedAnswer}</ReactMarkdown>
 }
 
 export default ProfessorAnswer
